1a6a9a74f1160c87353221eccd4934d4acaa403d
Nick <n.sychevskyi@gmail.com>
2019-08-28 13:50:46 +0300

    Wrap containers in HOC's to get loaded data

--- src/containers/form-fill-page/form-fill-page.js
+++ src/containers/form-fill-page/form-fill-page.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { getCurrForm } from 'reducers/forms';
-import { addFill, updateFill, getFormFillsCount } from 'reducers/fills';
+import { getOptionName, uniqueId } from 'utils';
+import { makeGetCurrForm, formsLoadedSelector } from 'reducers/forms';
+import { addFill, updateFill, formFillsSelector, formFillsTotalSelector } from 'reducers/fills';
 import { setRole } from 'reducers/auth';
+import withFormsData from 'hocs/with-forms-data/with-forms-data';
 import FormFillPageView from './views/form-fill-page-view';
 
 class FormFillPage extends Component {
@@ -13,11 +15,16 @@ class FormFillPage extends Component {
   };
 
   componentDidMount() {
+    const { loaded, fillPage } = this.props;
+
     this.props.setRole('guest');
-    this.props.fillPage.fields.forEach(item => this.handleChangeDataState(
-      item.name,
-      item.defaultOption || item.checked
-    ));
+
+    if (loaded) {
+      fillPage.fields.forEach(item => this.handleChangeDataState(
+        item.name,
+        item.defaultOption || item.checked
+      ));
+    }
   }
 
   toggleValue = value => {
@@ -50,25 +57,66 @@ class FormFillPage extends Component {
     Object.keys(this.state.data).forEach(item => this.handleChangeDataState(item, ''));
   };
 
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { fillPage, formFills, formFillsTotal, addFill, updateFill } = this.props;
+    const { data } = this.state;
+
+    const filledData = fillPage.fields
+      .reduce((res, field) => ({
+        ...res,
+        [field.label]: field.options
+          ? getOptionName(field.options, data[field.name])
+          : data[field.name] || ''
+      }), {});
+
+    const action = formFillsTotal ? updateFill : addFill;
+    const newFill = {
+      _id: {
+        $oid: fillPage._id.$oid
+      },
+      fills: [
+        ...formFills,
+        {
+          id: uniqueId(),
+          fields: filledData,
+        }
+      ]
+    };
+
+    action(newFill, fillPage._id.$oid);
+    this.toggleValue('successful');
+  };
+
   render() {
     return (
       <FormFillPageView
         {...this.props}
         {...this.state}
         resetForm={this.resetForm}
+        toggleValue={this.toggleValue}
         handleChange={this.handleChange}
         handleToggleData={this.handleToggleData}
-        toggleValue={this.toggleValue}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
 }
 
-const enhance = connect(
-  (state, props) => ({
+const makeMapStateToProps = () => {
+  const getCurrForm = makeGetCurrForm();
+
+  return (state, props) => ({
     fillPage: getCurrForm(state, props),
-    formFillsCount: getFormFillsCount(state, props),
-  }),
+    formFills: formFillsSelector(state, props),
+    formFillsTotal: formFillsTotalSelector(state, props),
+    loaded: formsLoadedSelector(state),
+  });
+};
+
+const enhance = connect(
+  makeMapStateToProps,
   {
     addFill,
     updateFill,
@@ -76,4 +124,6 @@ const enhance = connect(
   }
 );
 
-export default enhance(FormFillPage);
+const FormFillPageHoc = withFormsData(FormFillPage);
+
+export default enhance(FormFillPageHoc);
