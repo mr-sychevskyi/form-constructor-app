f9c1c3c0d840cda744a63f5ffabbb73f06ec6584
Nick <n.sychevskyi@gmail.com>
2019-09-02 06:42:53 +0300

    Add page-not-found styles. Add snackbar custom component.

--- src/components/page-not-found/page-not-found.js
+++ src/components/page-not-found/page-not-found.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import './page-not-found.scss';
 
-const PageNotFound = () => <h2 className="route route-notfound">Page not found</h2>;
+const PageNotFound = () => (
+  <div className="d-table">
+    <h2 className="d-table-cell route-notfound">Page not found.</h2>
+  </div>
+);
 
 export default PageNotFound;
