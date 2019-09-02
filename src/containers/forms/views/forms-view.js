46d7333c66a4ef362d8276a54f1794a6fbb6bdde
Nikolay Sychevskyi <n.sychevskyi@gmail.com>
2019-09-02 07:06:10 +0300

    Update forms-view.js

--- src/containers/forms/views/forms-view.js
+++ src/containers/forms/views/forms-view.js
@@ -1,65 +1,67 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { Header } from 'components';
 
 import './forms-view.scss';
 
 const FormsView = ({ forms, fillsTotalList, currCopiedId, handleCopied }) => (
   <>
-    <Header/>
+    {forms.length === 0
+      ? <h3 className="info-title 1">No forms are available!</h3>
+      : (
+        <>
+          <div className="forms">
+            <h3 className="form-title">Forms list</h3>
+            <ul className="forms-list">
+              {forms.map(form => {
+                const id = form._id.$oid;
 
-    <main className="main">
-      <div className="container ">
-        <div className="forms">
-          <h3 className="forms-list-title">Forms list</h3>
-          <ul className="forms-list">
-            {forms.map(form => (
-              // _id.$oid
-              <li key={form.id} className="forms-list-item">
-                <div className="form">
-                  <div className="forms-list-info">
-                    <span className="forms-list-text forms-list-name">{form.name}</span>
-                    <span className="forms-list-text forms-list-bx">Fields: {form.fields.length}</span>
-                    <span className="forms-list-text forms-list-bx">
-                      <span>Fills: {fillsTotalList[form.id] || 0}</span>
-                    </span>
-                    {fillsTotalList[form.id] && (
-                      <Link className="btn btn-link" to={`/fills/${form.id}`} replace>
-                        View fills
-                      </Link>
-                    )}
-                  </div>
+                return (
+                  <li key={id} className="forms-list-item">
+                    <div className="form">
+                      <div className="forms-list-info">
+                        <span className="forms-list-text forms-list-name">{form.name}</span>
+                        <span className="forms-list-text forms-list-bx">Fields: {form.fields.length}</span>
+                        <span className="forms-list-text forms-list-bx">
+                          <span>Fills: {fillsTotalList[id] || 0}</span>
+                        </span>
+                        {fillsTotalList[id] && (
+                          <Link className="btn btn-link" to={`/fills/${id}`} replace>
+                            View fills
+                          </Link>
+                        )}
+                      </div>
 
-                  <div className="forms-list-controls">
-                    {/* <Link className="forms-list-text" to={`/forms/active/${form.id}`}>Link to fill</Link> */}
+                      <div className="forms-list-controls">
+                        {/* <Link className="forms-list-text" to={`/forms/active/${id}`}>Link to fill</Link> */}
 
-                    {!fillsTotalList[form.id] && (
-                      <Link className="btn btn-link" title="Edit form" to={`/forms/${form.id}`} replace>
-                        <i className="btn__icon material-icons">edit</i>
-                      </Link>
-                    )}
+                        {!fillsTotalList[id] && (
+                          <Link className="btn btn-link" title="Edit form" to={`/forms/${id}`} replace>
+                            <i className="btn__icon material-icons">edit</i>
+                          </Link>
+                        )}
 
-                    <CopyToClipboard
-                      text={`${window.location.host}/#/forms/active/${form.id}`}
-                      onCopy={() => handleCopied(form.id)}
-                    >
-                      <button className="btn btn-link btn-clipboard" type="button">
-                        <span className="btn__text">Copy link to fill</span>
-                        <i className={`btn__icon material-icons ${currCopiedId === form.id ? 'copied' : ''}`}>
-                          done
-                        </i>
-                      </button>
-                    </CopyToClipboard>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <Link className="btn form__btn" to="/forms/new">Create form</Link>
-      </div>
-    </main>
+                        <CopyToClipboard
+                          text={`${window.location.host}/#/forms/active/${id}`}
+                          onCopy={() => handleCopied(id)}
+                        >
+                          <button className="btn btn-link btn-clipboard" type="button">
+                            <span className="btn__text">Copy link to fill</span>
+                            <i className={`btn__icon material-icons ${currCopiedId === id ? 'copied' : ''}`}>
+                              done
+                            </i>
+                          </button>
+                        </CopyToClipboard>
+                      </div>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <Link className="btn form__btn" to="/forms/new">Create form</Link>
+        </>
+      )}
   </>
 );
 
