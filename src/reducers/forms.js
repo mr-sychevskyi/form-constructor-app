d9bb1950822c834fbdc780fc22430217f1d1d837
Nikolay Sychevskyi <n.sychevskyi@gmail.com>
2019-09-02 07:01:01 +0300

    Update forms.js

--- src/reducers/forms.js
+++ src/reducers/forms.js
@@ -106,3 +106,4 @@ export const currFormId = (state, props) => props.match.params.id;
 export const makeGetCurrForm = () => createSelector(
   [formsDataSelector, currFormId],
   (forms, id) => forms.filter(form => form._id.$oid === id)[0],
+);
